50ccac895e2440e94a940bbd306b6d0291678608
Mineev <mineevahmed@gmail.com>
2016-05-20 01:28:44 +0800

    Secure and authenticate methods adds

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 ]
